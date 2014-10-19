2d135e98226b1381f8e35907719973bcb83153c3
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2014-10-19 19:10:56 +0100

    Production-ready logging

--- Main.hs
+++ Main.hs
@@ -3,11 +3,13 @@
 module Main where
 
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Logger (runStderrLoggingT)
+-- import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Monad.Logger (runNoLoggingT)
 import Controllers.Home (home, login, movies)
 import Data.Text
 import Database.Persist.Sqlite (withSqlitePool)
-import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+-- import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Network.Wai.Middleware.RequestLogger (logStdout)
 import Network.Wai.Middleware.Static (addBase, noDots, staticPolicy, (>->))
 import Network.Wai.Middleware.Gzip
 import Web.Scotty (middleware, scotty)
@@ -15,7 +17,7 @@ import Web.Scotty (middleware, scotty)
 import Models.Movies (runDB, mkMoviesDB)
 
 dbConnectionString :: Text
-dbConnectionString = ":memory:"
+dbConnectionString = "/tmp/movies.db"
 
 dbOpenConnections :: Int
 dbOpenConnections = 10
@@ -24,12 +26,12 @@ port :: Int
 port = 4000
 
 main :: IO ()
-main = runStderrLoggingT $ withSqlitePool dbConnectionString dbOpenConnections $ \pool -> liftIO $ do
+main = runNoLoggingT $ withSqlitePool dbConnectionString dbOpenConnections $ \pool -> liftIO $ do
     runDB pool $ liftIO $ do
       mkMoviesDB pool
       scotty port $ do
+        middleware logStdout
         middleware $ gzip $ def { gzipFiles = GzipCompress }
-        middleware logStdoutDev
         middleware $ staticPolicy (noDots >-> addBase "Static")
         home
         movies pool
